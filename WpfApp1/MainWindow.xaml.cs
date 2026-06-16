b9570a98b1dffa0444c97067e04447497e3bf9c3
m1tex <galov.kirya@gmail.com>
2026-06-16 07:19:10 +0300

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Random random = new Random();
+        private SecondWindow secondWindow;
+        private void KnockButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Открываем второе окно, если ещё не открыто
+            if (secondWindow == null || !secondWindow.IsVisible)
+            {
+                secondWindow = new SecondWindow();
+                secondWindow.Closed += (s, args) => secondWindow = null;
+                secondWindow.Show();
+            }
+
+            // Генерируем случайное событие (0, 1 или 2)
+            int scareType = random.Next(3);
+            switch (scareType)
+            {
+                case 0:
+                    PlayScream();
+                    break;
+                case 1:
+                    StartScaryAnimation();
+                    break;
+                case 2:
+                    ShowScaryMessage();
+                    break;
+            }
+        }
+
+        // 1. Крик – воспроизведение звука
+        private void PlayScream()
+        {
+            try
+            {
+                // Путь к файлу крика (поместите scream.wav в папку с приложением)
+                SoundPlayer player = new SoundPlayer("scream.wav");
+                player.Play();
+            }
+            catch
+            {
+                // Если файла нет – просто пискнем
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        // 2. Страшная анимация – тряска второго окна
+        private void StartScaryAnimation()
+        {
+            if (secondWindow != null && secondWindow.IsVisible)
+            {
+                secondWindow.StartShakeAnimation();
+            }
+        }
+
+        // 3. Сообщение
+        private void ShowScaryMessage()
+        {
+            MessageBox.Show("Бууууу! Напугался? 👻", "СТРАШНО!",
+                            MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
+
+
---
+++ WpfApp1/SecondWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+        // Метод для запуска анимации тряски
+        public void StartShakeAnimation()
+        {
+            // Создаём два параллельных таймлайна для тряски по X и Y
+            var storyboard = new Storyboard();
+
+            var shakeX = new DoubleAnimation
+            {
+                From = 0,
+                To = 10,
+                Duration = TimeSpan.FromMilliseconds(50),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(5) // 5 раз туда-сюда
+            };
+            Storyboard.SetTarget(shakeX, ShakeTransform);
+            Storyboard.SetTargetProperty(shakeX, new PropertyPath("X"));
+
+            var shakeY = new DoubleAnimation
+            {
+                From = 0,
+                To = 10,
+                Duration = TimeSpan.FromMilliseconds(50),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(5)
+            };
+            Storyboard.SetTarget(shakeY, ShakeTransform);
+            Storyboard.SetTargetProperty(shakeY, new PropertyPath("Y"));
+
+            storyboard.Children.Add(shakeX);
+            storyboard.Children.Add(shakeY);
+
+            // Заодно помигаем фоном
+            var flashAnimation = new ColorAnimation
+            {
+                From = Colors.DarkRed,
+                To = Colors.Black,
+                Duration = TimeSpan.FromMilliseconds(100),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(3)
+            };
+            Storyboard.SetTarget(flashAnimation, this);
+            Storyboard.SetTargetProperty(flashAnimation, new PropertyPath("Background.Color"));
+            storyboard.Children.Add(flashAnimation);
+
+            storyboard.Begin(this);
+        }
+    }
+}
